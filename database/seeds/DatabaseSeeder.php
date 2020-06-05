a4e7b51d9ad4bfde5ff5d5729ec206eabfd63fb0
Oladipupo Fredrick <fredrickksola@gmail.com>
2020-06-05 09:10:36 -0700

    fixes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-             DeliveryLocation::class
+             DeliveryLocation::class,
             UsersTableSeeder::class,
             ProductSeeder::class,
             OrderTableSeeder::class,
