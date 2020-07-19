183a42ba0fd41f2994ab0b0d31fb51dfaf6254ef
Oladipupo Fredrick <fredrickksola@gmail.com>
2020-07-19 10:12:23 -0700

    fixes

--- database/factories/DeliveryLocation.php
+++ database/factories/DeliveryLocation.php
@@ -6,8 +6,9 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\DeliveryLocation::class, function (Faker $faker) {
     return [
-        'name' => $faker->country(),
-        'description' => $faker->words(5, true),
+        'name' => $faker->randomElement(["Mainland A", "Island A", "Mainland B", "Island B"]),
+        'description' => $faker->randomElement(["Lagos island, ikoyi, Victoria Island, Lekki (phase1-vgc 1", "Ajah, badore, sangotedo, Obehi Lekki, epe 1", "gbagada, ikeja, Surulere, yaba, Oshodi, ojodu 3", "ikorodu,Apapa, festac, trade fair, Alaba, badagry 4"]),
         'price' => $faker->numberBetween(2000, 5000),
+        'status' => 1
     ];
 });
