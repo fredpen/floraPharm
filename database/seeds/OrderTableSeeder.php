6659b89a38a85c99df2dfe771edaf6af59d010bc
Oladipupo Fredrick <fredrickksola@gmail.com>
2020-07-22 13:47:56 -0700

    fixes

--- database/seeds/OrderTableSeeder.php
+++ database/seeds/OrderTableSeeder.php
@@ -11,9 +11,9 @@ class OrderTableSeeder extends Seeder
      */
     public function run()
     {
-        // factory(\App\Models\Order::class, 30)->create();
+        factory(\App\Models\Order::class, 30)->create();
 
-       // factory(\App\Models\OrderDetail::class, 30)->create();
+       factory(\App\Models\OrderDetail::class, 30)->create();
 
        factory(\App\Models\WishList::class, 30)->create();
     }
